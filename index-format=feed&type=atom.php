8448c56abee6c95a5c11eeebe8e6f3823385e1a6
drakearnowam <drakearnowam@allergist.com>
2020-06-10 15:26:25 +0300

    1

---
+++ index-format=feed&type=atom.php
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="ro-ro">
+	<title type="text">Home</title>
+	<subtitle type="text">Sala de Fitness a Universitatii Romano-Americane</subtitle>
+	<link rel="alternate" type="text/html" href="http://fitness.rau.ro"/>
+	<id>http://fitness.rau.ro/index.php</id>
+	<updated>2020-06-10T12:17:59+00:00</updated>
+	<generator uri="http://joomla.org" version="2.5">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://fitness.rau.ro/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Despre noi</title>
+		<link rel="alternate" type="text/html" href="http://fitness.rau.ro/index.php/78-articole-postate/79-despre-noi"/>
+		<published>2013-02-22T15:08:35+00:00</published>
+		<updated>2013-02-22T15:08:35+00:00</updated>
+		<id>http://fitness.rau.ro/index.php/78-articole-postate/79-despre-noi</id>
+		<author>
+			<name>Super User</name>
+			<email>gabi.negricea@rau.ro</email>
+		</author>
+		<summary type="html">&lt;p align=&quot;center&quot;&gt;
+	&lt;span style=&quot;font-size:14px;&quot;&gt;&lt;span style=&quot;font-family: times new roman,times,serif;&quot;&gt;&lt;span style=&quot;color:#ff0000;&quot;&gt;&lt;span style=&quot;font-size: 18px;&quot;&gt;&lt;strong&gt;Mentine-te in forma la Rau Fitness !&lt;/strong&gt;&lt;/span&gt;&lt;/span&gt;&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;
+	&amp;nbsp;&lt;/p&gt;
+&lt;p&gt;
+	&lt;span style=&quot;font-size:14px;&quot;&gt;&lt;span style=&quot;font-family: times new roman,times,serif;&quot;&gt;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; &lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p&gt;
+	Situata in incinta &lt;strong&gt;&lt;em&gt;Universitatii Romano&amp;ndash;Americana&lt;/em&gt;&lt;/strong&gt;, &lt;strong&gt;&amp;nbsp;Rau Fitness&amp;nbsp; &lt;/strong&gt;este locul unde te simti bine, lasand in urma stresul si problemele cotidiene!&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Intr-un ambient&amp;nbsp; mai mult decat placut, va asteptam sa va petreceti timpul impreuna cu noi! Aici puteti descoperi combinatia perfecta intre miscare, relaxare, si socializare!&lt;br /&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Sala noastra se intinde pe o suprafata de aproximativ 350 mp,&amp;nbsp; dispunand de numeroase dotari:&lt;/p&gt;
+&lt;ul&gt;
+	&lt;li&gt;
+		Sala de Fitness dotata cu aparatura de ultima generatie;&lt;/li&gt;
+	&lt;li&gt;
+		Sala Aerobic/Kangoo Jumps;&lt;/li&gt;
+	&lt;li&gt;
+		Infrasauna;&lt;/li&gt;
+	&lt;li&gt;
+		Vestiare dotate cu dusuri, grupuri sanitare ;&lt;/li&gt;
+	&lt;li&gt;
+		Aer conditionat;&lt;/li&gt;
+	&lt;li&gt;
+		Uscator de par;&lt;/li&gt;
+	&lt;li&gt;
+		Prosoape;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p style=&quot;margin-left:36.0pt;&quot;&gt;
+	&lt;strong&gt;I&lt;/strong&gt;&lt;strong&gt;n pretul abonamentului este inclusa si aparatura cardio: benzi, biciclete,stepper.&lt;/strong&gt;&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Toate acestea la preturi speciale, adaptate&amp;nbsp; nevoilor&amp;nbsp; studentilor, dar si celorlalti doritori!&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Pe langa toate acestea, &lt;strong&gt;Rau Fitness&amp;nbsp; &lt;/strong&gt;va recomanda o nutritie sanatoasa, combinata cu o savoare si un serviciu pe masura. In fiecare pauza pe care o veti face, veti avea posibilitatea sa va bucurati de&lt;strong&gt; suplimentele nutritive Redis&lt;/strong&gt;.&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;
+	&lt;strong&gt;&lt;em&gt;Suntem de parere ca o minte sanatoasa trebuie sa fie sustinuta de un corp sanatos, asadar va asteptam alaturi de noi pentru a incepe un stil de viata sanatos !&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;
+</summary>
+		<content type="html">&lt;p align=&quot;center&quot;&gt;
+	&lt;span style=&quot;font-size:14px;&quot;&gt;&lt;span style=&quot;font-family: times new roman,times,serif;&quot;&gt;&lt;span style=&quot;color:#ff0000;&quot;&gt;&lt;span style=&quot;font-size: 18px;&quot;&gt;&lt;strong&gt;Mentine-te in forma la Rau Fitness !&lt;/strong&gt;&lt;/span&gt;&lt;/span&gt;&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;
+	&amp;nbsp;&lt;/p&gt;
+&lt;p&gt;
+	&lt;span style=&quot;font-size:14px;&quot;&gt;&lt;span style=&quot;font-family: times new roman,times,serif;&quot;&gt;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; &lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p&gt;
+	Situata in incinta &lt;strong&gt;&lt;em&gt;Universitatii Romano&amp;ndash;Americana&lt;/em&gt;&lt;/strong&gt;, &lt;strong&gt;&amp;nbsp;Rau Fitness&amp;nbsp; &lt;/strong&gt;este locul unde te simti bine, lasand in urma stresul si problemele cotidiene!&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Intr-un ambient&amp;nbsp; mai mult decat placut, va asteptam sa va petreceti timpul impreuna cu noi! Aici puteti descoperi combinatia perfecta intre miscare, relaxare, si socializare!&lt;br /&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Sala noastra se intinde pe o suprafata de aproximativ 350 mp,&amp;nbsp; dispunand de numeroase dotari:&lt;/p&gt;
+&lt;ul&gt;
+	&lt;li&gt;
+		Sala de Fitness dotata cu aparatura de ultima generatie;&lt;/li&gt;
+	&lt;li&gt;
+		Sala Aerobic/Kangoo Jumps;&lt;/li&gt;
+	&lt;li&gt;
+		Infrasauna;&lt;/li&gt;
+	&lt;li&gt;
+		Vestiare dotate cu dusuri, grupuri sanitare ;&lt;/li&gt;
+	&lt;li&gt;
+		Aer conditionat;&lt;/li&gt;
+	&lt;li&gt;
+		Uscator de par;&lt;/li&gt;
+	&lt;li&gt;
+		Prosoape;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p style=&quot;margin-left:36.0pt;&quot;&gt;
+	&lt;strong&gt;I&lt;/strong&gt;&lt;strong&gt;n pretul abonamentului este inclusa si aparatura cardio: benzi, biciclete,stepper.&lt;/strong&gt;&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Toate acestea la preturi speciale, adaptate&amp;nbsp; nevoilor&amp;nbsp; studentilor, dar si celorlalti doritori!&lt;/p&gt;
+&lt;p&gt;
+	&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Pe langa toate acestea, &lt;strong&gt;Rau Fitness&amp;nbsp; &lt;/strong&gt;va recomanda o nutritie sanatoasa, combinata cu o savoare si un serviciu pe masura. In fiecare pauza pe care o veti face, veti avea posibilitatea sa va bucurati de&lt;strong&gt; suplimentele nutritive Redis&lt;/strong&gt;.&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;
+	&lt;strong&gt;&lt;em&gt;Suntem de parere ca o minte sanatoasa trebuie sa fie sustinuta de un corp sanatos, asadar va asteptam alaturi de noi pentru a incepe un stil de viata sanatos !&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;
+</content>
+		<category term="Special" />
+		<category term="Articole postate" />
+	</entry>
+</feed>
